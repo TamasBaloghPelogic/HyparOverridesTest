52d84ee8e4607dcee584f8fca94f8f4822b283a6
TamasBalogh <tamasbalogh95@gmail.com>
2023-03-21 09:24:27 +0100

    Overrides initial setup.

---
+++ OverrideTraining/README.md
@@ -0,0 +1,21 @@
+<img src="preview.png" width="512">
+
+# OverrideTraining
+
+Testing out overrides
+
+|Input Name|Type|Description|
+|---|---|---|
+|Length|number|The Length.|
+|Width|number|The Width.|
+
+
+<br>
+
+|Output Name|Type|Description|
+|---|---|---|
+
+
+<br>
+
+## Additional Information
---
+++ OverrideTraining/src/OverrideTraining.cs
@@ -0,0 +1,21 @@
+using Elements;
+using Elements.Geometry;
+using System.Collections.Generic;
+
+namespace OverrideTraining
+{
+      public static class OverrideTraining
+    {
+        /// <summary>
+        /// Testing out overrides
+        /// </summary>
+        /// <param name="model">The input model.</param>
+        /// <param name="input">The arguments to the execution.</param>
+        /// <returns>A OverrideTrainingOutputs instance containing computed results and the model with any new elements.</returns>
+        public static OverrideTrainingOutputs Execute(Dictionary<string, Model> inputModels, OverrideTrainingInputs input)
+        {
+            var output = new OverrideTrainingOutputs();
+            return output;
+        }
+      }
+}
